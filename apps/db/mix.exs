c10a53b684a925c3725670ac8e52839648f11f26
Alexander Tsirel <noma4i@gmail.com>
2017-09-16 20:52:42 +0800

    Disable DB start

--- apps/db/mix.exs
+++ apps/db/mix.exs
@@ -10,18 +10,18 @@ defmodule DB.Mixfile do
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       elixir: "~> 1.4",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      # build_embedded: Mix.env == :prod,
+      # start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
   def application do
     [
-      extra_applications: [
-        :logger
-      ],
-      mod: {DB.Application, []}
+      # extra_applications: [
+      #   :logger
+      # ],
+      # mod: {DB.Application, []}
     ]
   end
 
