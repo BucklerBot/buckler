2f2e9ef6585e6fd06b77122772db7db241dfd1a6
Alexander Tsirel <noma4i@gmail.com>
2017-09-18 10:17:48 +0800

    Use telegram token fetched from env

--- buckler_bot.eye.rb
+++ buckler_bot.eye.rb
@@ -9,6 +9,7 @@ Eye.app :buckler_bot do
   trigger :flapping, times: 10, within: 1.minute
 
   group 'elixir_release' do
+    env 'TELEGRAM_TOKEN' => TELEGRAM_TOKEN
     chain action: :restart, grace: 30.seconds
 
     stdall '/tmp/trash.log'
@@ -18,11 +19,11 @@ Eye.app :buckler_bot do
         pid_file "tmp/buckler_bot_#{port}.pid"
         daemonize true
 
-        start_command "/bin/su - #{USER} -c \"cd #{BUCKLER_APP_DIR}/rel/buckler_bot/bin && PORT=#{port} REPLACE_OS_VARS=true NODENAME=master_node_#{port} ./buckler_bot foreground\""
+        start_command "/bin/su - #{USER} -c \"cd #{BUCKLER_APP_DIR}/rel/buckler_bot/bin && PORT=#{port} DB_NAME=buckler TELEGRAM_TOKEN=#{TELEGRAM_TOKEN} REPLACE_OS_VARS=true NODENAME=master_node_#{port} ./buckler_bot foreground\""
 
         stop_signals [:QUIT, 3.seconds, :KILL]
 
-        restart_command "/bin/su - #{USER} -c \"cd #{BUCKLER_APP_DIR}/rel/buckler_bot/bin && PORT=#{port} REPLACE_OS_VARS=true NODENAME=master_node_#{port} ./buckler_bot stop\""
+        restart_command "/bin/su - #{USER} -c \"cd #{BUCKLER_APP_DIR}/rel/buckler_bot/bin && PORT=#{port} DB_NAME=buckler TELEGRAM_TOKEN=#{TELEGRAM_TOKEN} REPLACE_OS_VARS=true NODENAME=master_node_#{port} ./buckler_bot stop\""
 
         restart_grace 10.seconds
       end
