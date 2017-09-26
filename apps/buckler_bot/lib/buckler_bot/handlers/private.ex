5579c138940ecd5104c5335202140386b0b8f34a
Dmitry Rubinstein <rubinsteindb@gmail.com>
2017-09-26 17:18:17 +0300

    Adding basic bot instructions

--- apps/buckler_bot/lib/buckler_bot/handlers/private.ex
+++ apps/buckler_bot/lib/buckler_bot/handlers/private.ex
@@ -11,6 +11,31 @@ defmodule BucklerBot.Handlers.Private do
     request: %{"message" => %{"chat" => %{"id" => chat_id, "type" => "private"}, "text" => "/ping"}}
   }, _) do
     conn
-    |> send_message(chat_id, "pong")
+    |> send_message(chat_id, "BucklerBot is now working")
+  end
+
+  def call(conn = %Agala.Conn{
+    request: %{"message" => %{"chat" => %{"id" => chat_id, "first_name" => first_name, "type" => "private"}, "text" => "/start"}}
+  }, _) do
+    conn
+    |> send_message(
+      chat_id,
+      gettext(
+      """
+      Hello, %{first_name}!
+
+      I'm *BucklerBot*, and I'll defend your group or chat from dirsty spammers.
+
+      1. Add me as Administrator to your group.
+      2. Give me only two rights: to *Ban users* and to *Delete messages*
+      3. *???*
+      4. Enjoy!
+
+      If you have any questions or issues - welcome to our project's
+      repo - https://github.com/BucklerBot/buckler
+      """,
+      first_name: first_name),
+      parse_mode: "Markdown"
+    )
   end
 end
